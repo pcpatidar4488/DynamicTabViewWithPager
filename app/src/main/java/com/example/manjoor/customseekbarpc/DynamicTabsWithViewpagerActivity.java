71a29bbc8c1cd8334f9b8a525154ef4d90b3b95e
PUNAM CHAND <punam.chand@berylsystems.com>
2018-11-23 11:26:49 +0530

    TabLayout scrolling added

--- app/src/main/java/com/example/manjoor/customseekbarpc/DynamicTabsWithViewpagerActivity.java
+++ app/src/main/java/com/example/manjoor/customseekbarpc/DynamicTabsWithViewpagerActivity.java
@@ -1,17 +1,23 @@
 package com.example.manjoor.customseekbarpc;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DynamicTabsWithViewpagerActivity extends AppCompatActivity {
 
     TabLayout tabLayout;
     ViewPager viewPager;
     Toolbar toolbar;
-
+    int no_of_categories=-1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +26,12 @@ public class DynamicTabsWithViewpagerActivity extends AppCompatActivity {
 
         tabLayout=(TabLayout)findViewById(R.id.tabs);
         viewPager=(ViewPager)findViewById(R.id.viewpager);
+        no_of_categories=5;
 
-
-        for (int i = 0; i < 5; i++) {
-            tabLayout.addTab(tabLayout.newTab().setText("TAB " + String.valueOf(i + 1)));
-        }
-
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
-
+        // View pager setup function
+        setupViewPager(viewPager,15);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+        tabLayout.setupWithViewPager(viewPager);
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
@@ -46,7 +49,43 @@ public class DynamicTabsWithViewpagerActivity extends AppCompatActivity {
 
             }
         });
-
-
     }
+
+    private void setupViewPager(ViewPager viewPager,int mNumOfTabs) {
+        DynamicTabsWithViewpagerActivity.ViewPagerAdapter adapter = new DynamicTabsWithViewpagerActivity.ViewPagerAdapter(getSupportFragmentManager());
+        for (int i = 0; i < mNumOfTabs ; i++) {
+            adapter.addFragment(new DetailsFragment(), "Tab"+(i+1));
+        }
+        viewPager.setAdapter(adapter);
+    }
+
+    public class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
 }
